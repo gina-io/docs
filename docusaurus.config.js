9ceb5c8721f43971d35169261b5b2d624bbb1bd5
Martin Luther ETOUMAN <martin.etouman@freelancer.app>
2026-03-27 13:35:34 +0100

    Updated gina version to 0.1.8

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import readingTimePlugin from './src/remark/reading-time.js';
 
 // Updated automatically by gina's post_publish script on each release.
-const ginaVersion = '0.1.8-alpha.1';
+const ginaVersion = '0.1.8';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
